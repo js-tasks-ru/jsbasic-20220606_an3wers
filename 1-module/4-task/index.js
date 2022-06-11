aeb4a1f81691f0e4f0468f1a9f02af2200695201
Artem Zhuravlev <an3wers@gmail.com>
2022-06-11 21:33:13 +0500

    1 module task 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  const x1 = "1xBet";
+  const x2 = "XXX";
+  if (
+    str.toLowerCase().includes(x1.toLowerCase()) ||
+    str.toLowerCase().includes(x2.toLowerCase())
+  ) {
+    return true;
+  }
+  return false;
 }
