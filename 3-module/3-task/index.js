d35bd8e26a85418e451f6e7e73eeabcae5f2f3bf
Artem Zhuravlev <an3wers@gmail.com>
2022-06-19 12:25:39 +0500

    3 module task 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  const arr = str.split("-").map((el, index) => {
+    if (index !== 0) {
+      if (el.length) {
+        el = el.slice(0, 1).toUpperCase() + el.slice(1, el.length);
+      }
+    }
+    return el;
+  });
+  return arr.join("");
 }
