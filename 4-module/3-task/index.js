258b9c630e203f01fb89deec0144e1f62b3dedd7
Artem Zhuravlev <an3wers@gmail.com>
2022-06-23 20:00:03 +0500

    4 module task 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  const trs = table.querySelectorAll("tr");
+  for (let tr of trs) {
+    if (tr.cells[3].hasAttribute("data-available")) {
+      if (tr.cells[3].dataset.available === "true") {
+        tr.classList.add("available");
+      }
+
+      if (tr.cells[3].dataset.available === "false") {
+        tr.classList.add("unavailable");
+      }
+    } else {
+      tr.setAttribute("hidden", true);
+    }
+
+    if (tr.cells[2].textContent === "m") {
+      tr.classList.add("male");
+    }
+
+    if (tr.cells[2].textContent === "f") {
+      tr.classList.add("female");
+    }
+
+    if (tr.cells[1].textContent < 18) {
+      tr.style.textDecoration = "line-through";
+    }
+  }
 }
